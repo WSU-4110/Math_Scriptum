7ea4140a730b9cb152ed5db536f13fe6ab64ab05
SultanAdil <sultanadil21@gmail.com>
2019-10-24 01:25:39 -0400

    Save Progress Done
    The button Save Progress will save all text (in a txt file).

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -63,13 +63,14 @@ void MainWindow::on_pushButton_clicked()
 
 void MainWindow::on_selectSaveFileButton_clicked()
 {
-    // NOT DONE
+    /// The user selects the location (path) of the save file to open.
     QString fileName = QFileDialog::getOpenFileName(this, tr("Open Save File"),
                                                      "/home",
-                                                     tr("Document (*.docx)"));
+                                                     tr("Document (*.txt)"));
 
     /// SaveFile is a QFile object to check if a save file has been created by the user.
     QFile SaveFile(fileName);
+
     if (!SaveFile.open(QIODevice::ReadOnly | QIODevice::Text))
     {
         /// If no save file is selected, then this a message will pop up to warn the user.
@@ -77,28 +78,35 @@ void MainWindow::on_selectSaveFileButton_clicked()
         return;
     }
 
-    /// Savefile was not closed so the other functions should be able to write and read from it.
-    SaveFile.open(QIODevice::WriteOnly);
+    /// The file path (location) of the save file is set.
+    filePath = fileName;
+
+    /// SaveFile is closed.
+    SaveFile.close();
 }
 
 void MainWindow::on_createSaveFileButton_clicked()
 {
-    // NOT DONE
+    /// The location (path) of the save file is set.
     QString fileName = QFileDialog::getSaveFileName(this, tr("Create Save File"),
-                                 "/home/jana/untitled.docx",
-                                 tr("Document (*.docx)"));
+                                 "/home/jana/untitled.txt",
+                                 tr("Document (*.txt)"));
 
     /// SaveFile is a QFile object to check if a save file has been created by the user.
     QFile SaveFile(fileName);
-    if (!SaveFile.open(QIODevice::WriteOnly | QIODevice::Text))
+
+    if (!SaveFile.open(QIODevice::WriteOnly | QIODevice::Text | QIODevice::Append))
     {
         /// If no save file is create, then this a message will pop up to warn the user.
         QMessageBox::warning(this, "Warning!","No save file has been CREATED! Until a save file is created or selected, nothing will be saved!");
         return;
     }
 
-    /// Savefile is open and not closed so the other functions should be able to write to it.
-    SaveFile.open(QIODevice::WriteOnly);
+    /// The file path (location) of the save file is set.
+    filePath = fileName;
+
+    /// SaveFile is closed.
+    SaveFile.close();
 }
 
 void MainWindow::on_pasteFromFile_clicked()
@@ -134,5 +142,33 @@ void MainWindow::on_pasteFromFile_clicked()
 
 void MainWindow::on_saveProgress_clicked()
 {
+    /// Get the file path (location) of the save file.
+    QString filename = filePath;
 
+    /// If no save file is set, then this message will warn the user.
+    if (filename.isEmpty())
+    {
+        QMessageBox::warning(this, "Warning!","No save file has been set! Until a save file is created OR selected, nothing can be saved!");
+        return;
+    }
+
+    /// SaveFile is a QFile object to check if a save file has been created by the user.
+    QFile SaveFile(filename);
+
+    /// Open the file
+    if ( !SaveFile.open(QIODevice::WriteOnly | QIODevice::Text | QIODevice::Append))
+    {
+        /// If the file failed to open, then this message will warn the user.
+        QMessageBox::warning(this, "Warning!","Save file failed to open! Select or Create a save file and then try again!");
+        return;
+    }
+
+    /// out is a QTextStream object to write to the selected save file.
+    QTextStream out(&SaveFile);
+
+    /// Everything in the Graphical notepad area is saved as plain text.
+    out << ui->textEdit->toPlainText() << "\n";
+
+    /// SaveFile is closed.
+    SaveFile.close();
 }
