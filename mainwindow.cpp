7c1bd79116f17bfbf95f594e50a9b8e53a51eda9
Sishir9624 <55563910+Sishir9624@users.noreply.github.com>
2019-11-14 18:28:15 -0500

    Updated Limits Derivates and Integrals
    Resized Images so that they didn't run out of the screen area when clicked.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -292,9 +292,13 @@ void MainWindow::on_limitsButton_clicked()
 
     QImage img1("/Users/sishirbuddha/Desktop/Limits1.png");
     QPixmap pm = QPixmap::fromImage(img1);
+    pm = pm.scaled(800, 800, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+
 
     QImage img2("/Users/sishirbuddha/Desktop/Limits2.png");
     QPixmap pm2 = QPixmap::fromImage(img2);
+    pm2 = pm2.scaled(800, 800, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+
 
     QMessageBox displayLimits;
     QMessageBox displayLimits2;
@@ -303,12 +307,14 @@ void MainWindow::on_limitsButton_clicked()
     displayLimits.setText("Limits Page One");
     displayLimits.setIconPixmap(pm);
     displayLimits.icon();
+    displayLimits.setBaseSize(1000,1000);
     displayLimits.exec();
 
     displayLimits2.setWindowTitle("Limits Page Two");
     displayLimits2.setText("Limits Page Two");
     displayLimits2.setIconPixmap(pm);
     displayLimits2.icon();
+    displayLimits2.setBaseSize(1000,1000);
     displayLimits2.exec();
 
     return;
@@ -319,15 +325,22 @@ void MainWindow::on_derivativesButton_clicked()
 
     QImage img1("/Users/sishirbuddha/Desktop/Deriv1.png");
     QPixmap pm = QPixmap::fromImage(img1);
+    pm = pm.scaled(800, 800, Qt::KeepAspectRatio, Qt::SmoothTransformation);
 
     QImage img2("/Users/sishirbuddha/Desktop/Deriv2.png");
     QPixmap pm2 = QPixmap::fromImage(img2);
+    pm2 = pm2.scaled(800, 800, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+
 
     QImage img3("/Users/sishirbuddha/Desktop/Deriv3.png");
     QPixmap pm3 = QPixmap::fromImage(img3);
+    pm3 = pm3.scaled(800, 800, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+
 
     QImage img4("/Users/sishirbuddha/Desktop/Deriv4.png");
     QPixmap pm4 = QPixmap::fromImage(img4);
+    pm4 = pm4.scaled(800, 800, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+
 
     QMessageBox displayDerivatives;
     QMessageBox displayDerivatives2;
@@ -339,24 +352,28 @@ void MainWindow::on_derivativesButton_clicked()
     displayDerivatives.setText("Derivatives Page One");
     displayDerivatives.setIconPixmap(pm);
     displayDerivatives.icon();
+    displayDerivatives.setBaseSize(1000,1000);
     displayDerivatives.exec();
 
     displayDerivatives2.setWindowTitle("Derivatives Page Two");
     displayDerivatives2.setText("Derivatives Page Two");
     displayDerivatives2.setIconPixmap(pm2);
     displayDerivatives2.icon();
+    displayDerivatives2.setBaseSize(1000,1000);
     displayDerivatives2.exec();
 
     displayDerivatives3.setWindowTitle("Derivatives Page Three");
     displayDerivatives3.setText("Derivatives Page Three");
     displayDerivatives3.setIconPixmap(pm3);
     displayDerivatives3.icon();
+    displayDerivatives3.setBaseSize(1000,1000);
     displayDerivatives3.exec();
 
     displayDerivatives4.setWindowTitle("Derivatives Page Four");
     displayDerivatives4.setText("Derivatives Page Four");
     displayDerivatives4.setIconPixmap(pm4);
     displayDerivatives4.icon();
+    displayDerivatives4.setBaseSize(1000,1000);
     displayDerivatives4.exec();
 
     return;
@@ -367,19 +384,29 @@ void MainWindow::on_integralsButton_clicked()
 {
     QImage img1("/Users/sishirbuddha/Desktop/Int1.png");
     QPixmap pm = QPixmap::fromImage(img1);
+        pm = pm.scaled(800, 800, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+
 
     QImage img2("/Users/sishirbuddha/Desktop/Int2.png");
     QPixmap pm2 = QPixmap::fromImage(img2);
+        pm2 = pm2.scaled(800, 800, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+
 
     QImage img3("/Users/sishirbuddha/Desktop/Int3.png");
     QPixmap pm3 = QPixmap::fromImage(img3);
+        pm3 = pm3.scaled(800, 800, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+
 
     QImage img4("/Users/sishirbuddha/Desktop/Int4.png");
     QPixmap pm4 = QPixmap::fromImage(img4);
+        pm4 = pm4.scaled(800, 800, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+
 
     QImage img5("/Users/sishirbuddha/Desktop/Int5.png");
     QPixmap pm5 = QPixmap::fromImage(img5);
+    pm5 = pm5.scaled(800, 800, Qt::KeepAspectRatio, Qt::SmoothTransformation);
 
+    
     QMessageBox displayIntegrals;
     QMessageBox displayIntegrals2;
     QMessageBox displayIntegrals3;
@@ -390,30 +417,40 @@ void MainWindow::on_integralsButton_clicked()
     displayIntegrals.setText("Integrals Page One");
     displayIntegrals.setIconPixmap(pm);
     displayIntegrals.icon();
+        displayIntegrals.setBaseSize(1000,1000);
+
     displayIntegrals.exec();
 
     displayIntegrals2.setWindowTitle("Integrals Page Two");
     displayIntegrals2.setText("Integrals Page Two");
     displayIntegrals2.setIconPixmap(pm2);
     displayIntegrals2.icon();
+           displayIntegrals2.setBaseSize(1000,1000);
+
     displayIntegrals2.exec();
 
     displayIntegrals3.setWindowTitle("Integrals Page Three");
     displayIntegrals3.setText("Integrals Page Three");
     displayIntegrals3.setIconPixmap(pm3);
     displayIntegrals3.icon();
+            displayIntegrals3.setBaseSize(1000,1000);
+
     displayIntegrals3.exec();
 
     displayIntegrals4.setWindowTitle("Integrals Page Four");
     displayIntegrals4.setText("Integrals Page Four");
     displayIntegrals4.setIconPixmap(pm4);
     displayIntegrals4.icon();
+            displayIntegrals4.setBaseSize(1000,1000);
+
     displayIntegrals4.exec();
 
     displayIntegrals5.setWindowTitle("Integrals Page Five");
     displayIntegrals5.setText("Integrals Page Five");
     displayIntegrals5.setIconPixmap(pm);
     displayIntegrals5.icon();
+            displayIntegrals5.setBaseSize(1000,1000);
+
     displayIntegrals5.exec();
 
     return;
