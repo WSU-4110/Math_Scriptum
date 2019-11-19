ddcbb217dbe416f0971d42aee4d1517ca26e7da2
Abe33O1 <abdella.albadani@wayne.edu>
2019-11-19 00:32:47 -0500

    updated UI for new window and fixed shaping bugs

--- shapewindow.cpp
+++ shapewindow.cpp
@@ -1,6 +1,7 @@
 #include "shapewindow.h"
 #include "ui_shapewindow.h"
-
+#include "shapearea.h"
+#include "ShapeWindow.h"
 ShapeWindow::ShapeWindow(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ShapeWindow)
@@ -11,4 +12,116 @@ ShapeWindow::ShapeWindow(QWidget *parent) :
 ShapeWindow::~ShapeWindow()
 {
     delete ui;
+}
+
+void ShapeWindow::update_UserInterface()
+{
+    this->ui->sCount->setValue(this->ui->Shapearea->step_counter());
+    this->ui->sScale->setValue(this->ui->Shapearea->scale());
+    this->ui->sInterval->setValue(this->ui->Shapearea->length());
+}
+
+void ShapeWindow::on_sCount_valueChanged(int arg1)
+{
+    this->ui->Shapearea->set_step_counter(arg1);
+}
+
+void ShapeWindow::on_sInterval_valueChanged(double arg1)
+{
+    this->ui->Shapearea->set_length_of_shape(arg1);
+}
+
+void ShapeWindow::on_sScale_valueChanged(double arg1)
+{
+    this->ui->Shapearea->set_scale_size(arg1);
+}
+
+
+void ShapeWindow::on_shape12Button_clicked()
+{
+    this->ui->Shapearea->set_up_Shape(ShapeArea::shape12);
+    this->ui->Shapearea->repaint();
+    update_UserInterface();
+}
+
+void ShapeWindow::on_shape11Button_clicked()
+{
+    this->ui->Shapearea->set_up_Shape(ShapeArea::shape11);
+    this->ui->Shapearea->repaint();
+    update_UserInterface();
+}
+
+void ShapeWindow::on_shape10Button_clicked()
+{
+    this->ui->Shapearea->set_up_Shape(ShapeArea::shape10);
+    this->ui->Shapearea->repaint();
+    update_UserInterface();
+}
+
+void ShapeWindow::on_line_Button_clicked()
+{
+    this->ui->Shapearea->set_up_Shape(ShapeArea::Lines);
+    this->ui->Shapearea->repaint();
+    update_UserInterface();
+}
+
+void ShapeWindow::on_huyg_Button_clicked()
+{
+    this->ui->Shapearea->set_up_Shape(ShapeArea::Huygens);
+    this->ui->Shapearea->repaint();
+    update_UserInterface();
+}
+
+void ShapeWindow::on_invertedCircle_Button_clicked()
+{
+    this->ui->Shapearea->set_up_Shape(ShapeArea::Line);
+    this->ui->Shapearea->repaint();
+    update_UserInterface();
+}
+
+void ShapeWindow::on_hypo_Button_clicked()
+{
+    this->ui->Shapearea->set_up_Shape(ShapeArea::Hype);
+    this->ui->Shapearea->repaint();
+    update_UserInterface();
+}
+
+void ShapeWindow::on_hexagon_Button_clicked()
+{
+    this->ui->Shapearea->set_up_Shape(ShapeArea::hexagon);
+        this->ui->Shapearea->repaint();
+        update_UserInterface();
+}
+
+void ShapeWindow::on_ellipse_Button_clicked()
+{
+    this->ui->Shapearea->set_up_Shape(ShapeArea::Ellipse);
+    this->ui->Shapearea->repaint();
+    update_UserInterface();
+}
+
+void ShapeWindow::on_circle_Button_clicked()
+{
+    this->ui->Shapearea->set_up_Shape(ShapeArea::Circle);
+    this->ui->Shapearea->repaint();
+    update_UserInterface();
+}
+
+void ShapeWindow::on_astroid_Button_clicked()
+{
+    this->ui->Shapearea->set_up_Shape(ShapeArea::Astroid);
+    this->ui->Shapearea->repaint();
+    update_UserInterface();
+
+}
+
+void ShapeWindow::on_CycloidButton_clicked()
+{
+    ///set up the shape call it from the shapearea class
+    this->ui->Shapearea->set_up_Shape(ShapeArea::Cycloid);
+    ///repaint the area
+    this->ui->Shapearea->repaint();
+    ///display the new shape
+    update_UserInterface();
+
 }
--- shapewindow.h
+++ shapewindow.h
@@ -15,6 +15,40 @@ public:
     explicit ShapeWindow(QWidget *parent = nullptr);
     ~ShapeWindow();
 
+private slots:
+
+    void on_sCount_valueChanged(int arg1);
+    void on_sInterval_valueChanged(double arg1);
+    void on_sScale_valueChanged(double arg1);
+
+    void on_shape12Button_clicked();
+
+
+    void on_shape11Button_clicked();
+
+    void on_shape10Button_clicked();
+
+    void on_line_Button_clicked();
+
+    void on_huyg_Button_clicked();
+
+    void on_invertedCircle_Button_clicked();
+
+    void on_hypo_Button_clicked();
+
+    void on_hexagon_Button_clicked();
+
+    void on_ellipse_Button_clicked();
+
+    void on_circle_Button_clicked();
+
+    void on_astroid_Button_clicked();
+
+    void on_CycloidButton_clicked();
+
+private:
+    void update_UserInterface ();
+
 private:
     Ui::ShapeWindow *ui;
 };
