0f6fffe91b19eb9f2030ba6d2637d6f5153844b9
Sishir9624 <55563910+Sishir9624@users.noreply.github.com>
2019-11-18 19:59:02 -0500

    Update equationsarea.h

--- equationsarea.h
+++ equationsarea.h
@@ -1,7 +1,12 @@
 #ifndef EQUATIONSAREA_H
 #define EQUATIONSAREA_H
-
+#include <QPaintEvent>
+#include <QPainter>
+#include <math.h>
 #include <QWidget>
+#include <QColor>
+#include <QSize>
+
 
 class EquationsArea : public QWidget
 {
@@ -9,9 +14,53 @@ class EquationsArea : public QWidget
 public:
     explicit EquationsArea(QWidget *parent = nullptr);
 
-signals:
+    ///Enumeration of graph type that will be used in switch case to draw matching graph type
+    enum graph_type
+    {
+        Line,
+        Parabola,
+        Sqrt,
+        XReciprical,
+        xAxis,
+        yAxis
+        };
 
-public slots:
+    ///setter that will set graph and load changes to widget on main window
+    void set_up_Graph(graph_type graph)
+    {
+        Graph = graph;
+        load_graph_change();
+    }
+
+protected:
+    ///Function that will draw graph selected in equationsarea widget on main window
+    void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
+
+private:
+    ///each function will draw a graph defined by the math functions they represent
+    QPointF drawLine(float t);
+    QPointF drawParabola(float t);
+    QPointF drawSQRT(float t);
+    QPointF drawXReciprical(float t);
+    QPointF drawXAxis(float t);
+    QPointF drawYAxis(float t);
+    QPointF drawGeneral(float t);
+
+    ///Function to set graph scale, length of interval and step count
+    void load_graph_change();
+
+    ///use Qcolor class to allow the background color to be changed
+    QColor background_color;
+
+    ///set up the color of the shape
+    QColor graph_color;
+
+    ///initialize enum graph type variable
+    graph_type Graph;
+
+    float length_of_Interval;
+    float Scale;
+    int step_count_of_shape;
 };
 
 #endif // EQUATIONSAREA_H
