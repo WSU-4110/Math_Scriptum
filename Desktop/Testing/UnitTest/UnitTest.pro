6fa191754c39772f7d625f2f84bf39504ce76385
Abe33O1 <abdella.albadani@wayne.edu>
2019-11-27 02:02:26 -0500

    Finished all unit testing for the following 5 functions shape10: Test will pass if t in cos is greater then sin t so the 2D shape will adjust correctly, else fail shape11: Test will pass if cos(t) is equal to 1 allowing the 2D shape to redraw without going out of bounds (greater then 1),else fail shape12: Test will passs if x is greater then sin(t) in real time when drawing shape (real time here is under 4ms), else fail testLines: test will pass of when drawing lines Pi is and is always greater then sin and cos of t for all variations of the shape made by the user, else fail testHex: test will pass if the basic edge of the shape stays consistent with the both edges at all times, preventing overflow of shape scalers, else fail

--- Desktop/Testing/UnitTest/UnitTest.pro
+++ Desktop/Testing/UnitTest/UnitTest.pro
@@ -5,3 +5,6 @@ CONFIG += qt warn_on depend_includepath testcase
 TEMPLATE = app
 
 SOURCES +=  tst_test.cpp
+
+HEADERS += \
+    ../Math_Scriptum/shapearea.h
--- Desktop/Testing/UnitTest/tst_test.cpp
+++ Desktop/Testing/UnitTest/tst_test.cpp
@@ -1,4 +1,6 @@
 #include <QtTest>
+#include <QtTest/QtTest>
+#include <QLine>
 
 // add necessary includes here
 
@@ -11,7 +13,11 @@ public:
     ~test();
 
 private slots:
-    void test_case1();
+    void test_Shape10();
+    void test_Shape12();
+    void test_Shape11();
+    void testHex();
+    void testLines();
 
 };
 
@@ -25,10 +31,67 @@ test::~test()
 
 }
 
-void test::test_case1()
+void test::test_Shape10()
 {
-    QString str = "Helli";
-    QCOMPARE(str, QString("Hello"));
+
+    float t = 0;
+    float cos_t = cos(t);
+    float sin_t = sin(t);
+    float x = 6 * cos_t * cos_t * cos_t * cos_t;
+    float y = 6 * sin_t * sin_t * sin_t * sin_t;
+    QVERIFY2(cos_t > sin_t, "SINE is smaller then COSINE");
+}
+
+void test::test_Shape11()
+{
+    float t = 0;
+    float vertex = 15;
+    float secondVertex = 4;
+
+    float x = vertex * cos (t) - secondVertex * cos (vertex / secondVertex * t);
+    float y = vertex * sin (t) - secondVertex * sin (vertex / secondVertex * t);
+    QCOMPARE(1, cos(t));
+
+
+}
+
+void test::test_Shape12()
+{
+    float t = 0.8490238;
+    float t1 = 9;
+    float t2 = 4;
+    float t3 = 9;
+
+    float x = (t1 - t2) * cos (t) + t3 * cos (t * ((t1 - t2) / t2));
+    float y = (t1 - t2) * sin (t) - t3 * sin (t * ((t1 - t2) / t2));
+    QTRY_VERIFY2_WITH_TIMEOUT(x > sin(t),"SHOULD BE UNDER 4MS",5);
+
+}
+
+void test::testLines()
+{
+    float t = 1;
+    float PI = 3.14159265358979323846;
+    float t1 = 19;
+    float t2 = 5;
+    float t3 = 19;
+
+    float x = (t1 - t2) * cos (t) + t3 * cos (t * ((t1 - t2) / t2));
+    float y = (t1 - t2) * sin (t) - t3 * sin (t * ((t1 - t2) / t2));
+
+   QTRY_VERIFY2(PI < x || PI < y, "PI SHOULD BE SMALLER THEN X AN Y IN TERMS OF SIN AND COS");
+
+}
+
+void test::testHex()
+{
+    float t = 1;
+    float basicEdge = 0.841471;
+    float edge1 = 2;
+    float edge2 = 1;
+    QPointF(edge1*cos(t), edge2*sin(t));
+    QTRY_COMPARE_WITH_TIMEOUT(basicEdge, (edge1*cos(t), edge2*sin(t)), 4);
+
 }
 
 QTEST_APPLESS_MAIN(test)
